7181d2bbd321cf39a818659b2d6170e35ebf466a
Luke <luke.arduini@me.com>
2012-09-21 01:17:00 -0400

    [ux] updated with more speakers & hotel details

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 var union    = require('union')
   , ecstatic = require('ecstatic')
   , director = require('director')
-  , nano     = require('nano')('https://empirejs.iriscouch.com')
 
 var router    = new director.http.Router()
-  , empire_db = nano.db.use('proposals')
-  , numbers_db = nano.db.use('numbers')
 
 var server = union.createServer({
   before: [
@@ -14,20 +11,6 @@ var server = union.createServer({
       router.dispatch(req,res)
     }
   ]
-})
-
-router.post(/phone/, function () {
-  var self = this
-  var data = this.req.body
-
-  numbers_db.insert({number: data}, function (err, body, headers) {
-    self.res.writeHead(200, { 'Content-Type': 'text/plain' })
-    if (err) {
-      self.res.end('Error submitting phone number. Please try again later.')
-      return
-    }
-    self.res.end('Phone Number Submitted')
-  })
 })
 
 server.listen(8080)
